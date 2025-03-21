41d24d9bfc09dd9714e45b52b7ad2609a09fe65c
ROHIT3496484 <rohitrana349@gmail.com>
2025-03-22 00:43:33 +0530

    final

--- src/Star-rating.js
+++ src/Star-rating.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 const StarRating = ({starcount}) => {
+    const [starvalue, setStarvalue] = useState();
+    const [mouseenter, setMouseenter] = useState(0);
     
   return (
     <div>
-      new Array(starcount).fill(0).map((val, index)=>{
+      {new Array(starcount).fill(0).map((val, index)=>{
+        //here we created new array for the all indexes
         return(
-            
+            <span key={index} className={mouseenter === 0 /*used for if we are selected star value and hover to minimum of selected */ & index < starvalue /*for the star selection */ || index < mouseenter ? "gold" : " "} onClick={()=>setStarvalue(index+1)}
+            onMouseEnter={()=> setMouseenter(index+1)}
+            onMouseOut={()=>setMouseenter(0)}
+            >
+                &#9734;
+            </span>
         )
-      })
+      })}
     </div>
   )
 }
