fd356f3e6a48ef6424fd156dcf79aefac6d0510c
ROHIT3496484 <rohitrana349@gmail.com>
2025-03-22 00:04:37 +0530

    hi

---
+++ src/Star-rating.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const StarRating = ({starcount}) => {
+    
+  return (
+    <div>
+      new Array(starcount).fill(0).map((val, index)=>{
+        return(
+            
+        )
+      })
+    </div>
+  )
+}
+
+export default StarRating
